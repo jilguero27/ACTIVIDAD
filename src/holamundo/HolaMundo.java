d852aaec2a25a77f8156aea6a593d5c11e917ea3
Joselyn <Joselyn@JJ>
2018-02-27 17:33:31 +0100

    Segundo comentario de Jose

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -17,6 +17,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         // Jose
         System.out.println("Primer comentario de Jose");
+        //Segundo cambio de Jose
+        System.out.println("Segundo comentario de Jose");
     }
     
 }
